bd0a4fc8049a072927963e26ed3e18b6120b6ebb
lollipopnougat <ab2defg145@gmail.com>
2019-09-03 22:19:56 +0800

    是新界面哟

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@
           <a class="headlink">
             <img src="./assets/chdns-logo.png"
                  class="logo" />
-
+            <img src="./assets/chdns-logos.png"
+                 class="logos"/>
           </a>
           <!--<span class="logo-headline">网络安全协会</span>
 
@@ -19,7 +20,8 @@
               <el-dropdown-item>删除</el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown> -->
-          <el-menu :default-active="activeIndex"
+          <el-menu :default-active="this.$route.path"
+                   router-mode="horizontal"
                    class="el-menu-demo"
                    mode="horizontal"
                    @select="handleSelect">
@@ -31,8 +33,23 @@
               <el-menu-item index="2-3">宣传部</el-menu-item>
               <el-menu-item index="2-4">组织部</el-menu-item>
             </el-submenu>
-            <el-menu-item index="3"><a href="https://github.com"
-                 target="_blank">Github</a></el-menu-item>
+            <el-menu-item index="3">
+              <a href="https://github.com"
+                 target="_blank">
+                <svg aria-labelledby="simpleicons-github-dark-icon"
+                     lang=""
+                     role="img"
+                     viewBox="0 0 24 24"
+                     class="github"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <title id="simpleicons-github-icon"
+                         lang="en">
+                    GitHub Dark icon
+                  </title>
+                  <path fill="#7F8C8D"
+                        d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"></path>
+                </svg>Github</a>
+            </el-menu-item>
           </el-menu>
           <div class="line"></div>
         </el-container>
@@ -47,11 +64,6 @@
 <script>
 export default {
   name: 'App',
-  data () {
-    return {
-      activeIndex: '1'
-    };
-  },
   methods: {
     handleSelect (key, keyPath) {
       console.log(key, keyPath);
@@ -67,10 +79,35 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin: 0;
 }
-.logo {
-  height: 100%;
-  width: auto;
+
+@media screen and (min-width: 620px) {
+  .logo {
+    height: 100%;
+    width: auto;
+  }
+  .logos {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 620px) and (min-width: 412px) {
+  .logo {
+    display: none;
+  }
+  .logos {
+    height: 100%;
+    width: auto;
+  }
+}
+@media screen and (max-width: 412px){
+  .logo {
+    display: none;
+  }
+  .logos {
+    display: none;
+  }
 }
 .headerd {
   height: 64px;
@@ -88,5 +125,14 @@ export default {
 .el-menu-demo {
   float: right;
   height: 100%;
+}
+
+.github {
+  width: 30px;
+  height: auto;
+  margin-right: 5px;
+}
+.el-main {
+  padding: 0 !important;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,131 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="/hello"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <h2>长安大学</h2>
+    <h1 class="topmargin">网络安全协会</h1>
+    <img src="../assets/nmap.png"
+         class="nmap" />
+    <div class="pblock">
+      <div class="block">
+        <span class="demonstration">默认 Hover 指示器触发</span>
+        <el-carousel height="150px"
+                     interval="3000"
+                     type="card">
+          <el-carousel-item v-for="item in 4"
+                            :key="item">
+            <h3 class="small">{{ item }}</h3>
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+    </div>
+
+    <p> </p>
+    <div class="pblock">
+      <el-popover placement="top-start"
+                  title="QQ迎新群   微信公众号"
+                  width="520"
+                  trigger="hover">
+        <div class="iblock">
+          <img src="../assets/QQR.png"
+               class="QR" />
+          <img src="../assets/WQR.png"
+               class="QR" /></div>
+        <el-button slot="reference">点击加群或关注微信公众号</el-button>
+      </el-popover>
+    </div>
+    <p>长 </p>
+    <p>大 </p>
+    <p>网 </p>
+    <p>络 </p>
+    <p>安 </p>
+    <p>全 </p>
+    <p>协 </p>
+    <p>会 </p>
+    <footer>2019 &copy; 长安大学网络安全协会 </footer>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: '欢迎访问'
-    };
-  }
+  name: 'HelloWorld'
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.cloud {
+  width: 100%;
+  height: auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+h1 {
+  font-size: 45px;
 }
-li {
+h2 {
+  font-size: 30px;
+  color: #909399;
+  margin-bottom: 10px;
+  margin-top: 4px;
+  padding-top: 20px;
+}
+.topmargin {
+  margin-top: 10px;
+}
+.hello {
+  background-image: url('../assets/bg2.png');
+  background-size: cover;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  zoom: 1;
+  background-color: #fff;
+  background-repeat: no-repeat;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -o-background-size: cover;
+  background-position: center 0;
+}
+.iblock {
   display: inline-block;
-  margin: 0 10px;
+  text-align: center;
 }
-a {
-  color: #42b983;
+.nmap {
+  width: 50%;
+  height: auto;
+}
+@media screen and (min-width: 524px) {
+  .QR {
+    width: 200px;
+    height: auto;
+  }
+}
+
+@media screen and (max-width: 523px) {
+  .QR {
+    width: 50%;
+    height: auto;
+    display: inline;
+  }
+}
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+  opacity: 0.75;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+  opacity: 0.75;
+}
+
+.pblock {
+  text-align: center;
+  display: inline-block;
+  width: 80%;
 }
 </style>
